9f3be03af1716d2c28ee68cd5d3c6b0069b5d886
HarikaValikala <harikavalikala521@gmail.com>
2021-07-29 18:12:02 +0530

    Nav Link and importance of Routes order

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
+
 import EmployeePage from './components/EmployeePage';
 import DepartmentPage from './components/DepartmentPage';
 import ProjectPage from './components/ProjectPage';
+import InvalidPath from './components/NotFound';
+
+import './App.css';
 
 function App() {
   return (
@@ -12,14 +16,15 @@ function App() {
         <h2>Welcome to App Component....</h2>
         <ul>
           <li><Link to="/">Employees</Link></li>
-          <li><Link to="/departments">Departments</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
+          <li><NavLink to="/departments" activeClassName="testClass">Departments</NavLink></li>
+          <li><NavLink to="/projects" activeClassName="testClass">Projects</NavLink></li>
         </ul>
         <Switch>
           <Route exact path="/" component={EmployeePage}></Route>
           <Route path="/departments" component={DepartmentPage}></Route>
           <Route path="/projects" component={ProjectPage}></Route>
           <Route path="/departments" component={EmployeePage}></Route>
+          <Route component={InvalidPath}></Route>
         </Switch>
         
       </div>
